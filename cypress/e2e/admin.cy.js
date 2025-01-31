8c578691eac98f6046802059acef40b4fa5fe51e
James Njuguna <jamesnjugunan998@gmail.com>
2025-01-31 16:08:22 +0300

    added the admin tests

---
+++ cypress/e2e/admin.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="Cypress"/>
+
+describe('Admin Panel Tests', () => {
+  
+    const adminCredentials = {
+      email: 'jimmy@gmail.com',
+      password: '12345'
+    };
+  
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
+  
+    const loginAsAdmin = () => {
+      cy.get('[data-cy=admin-input-email]').type(adminCredentials.email);
+      cy.get('[data-cy=admin-input-password]').type(adminCredentials.password);
+      cy.get('form#adminLoginForm').submit();
+      cy.get('#adminPanel').should('be.visible'); 
+    };
+  
+    it('Should log in as admin', () => {
+      loginAsAdmin();
+    });
+  
+    it('Should add a new product', () => {
+      loginAsAdmin();
+  
+      cy.get('.btn-primary').contains('Add Product').click();
+  
+      cy.get('[data-cy=product-input-name]').type('Product 1');
+      cy.get('[data-cy=product-input-price]').type('30000');
+      
+      cy.get('[data-cy=product-btn-submit]').click();
+  
+      cy.get('#productsList').should('contain', 'Product 1');
+    });
+  
+    it('Should verify that the product was added', () => {
+      loginAsAdmin();
+  
+      cy.get('.btn-primary').contains('View Products').click();
+      
+      cy.get('#productsList').should('contain', 'Product 1');
+    });
+  });
+  
--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,56 +1,58 @@
 /// <reference types="Cypress"/>
 
-describe('E-commerce Application Tests', () => {
-  
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-    });
-  
-    it('Should register a new user', () => {
-      cy.get('[data-cy="register-btn-submit"]').click();
-      cy.get('[data-cy="register-input-name"]').type('jonathan ndambuki');
-      cy.get('[data-cy="register-input-email"]').type('jonah@gmail.com');
-      cy.get('[data-cy="register-input-password"]').type('1234');
-      cy.get('[data-cy="register-btn-submit"]').click();
-      
-      cy.get('#userPanel').should('be.visible');
-    });
-  
-    it('Should log in an existing user', () => {
-      cy.get('[data-cy="register-btn-submit"]').click();
-      cy.get('[data-cy="register-input-name"]').type('jonathan ndambuki');
-      cy.get('[data-cy="register-input-email"]').type('jonah@gmail.com');
-      cy.get('[data-cy="register-input-password"]').type('1234');
-      cy.get('[data-cy="register-btn-submit"]').click();
-  
-      cy.get('[data-cy="login-input-email"]').type('jonah@gmail.com.com');
-      cy.get('[data-cy="login-input-password"]').type('1234');
-      cy.get('[data-cy="login-btn-submit"]').click();
-  
-      cy.get('#userPanel').should('be.visible');
-    });
-  
-    it('Should add items to the cart', () => {
-      cy.get('[data-cy="login-input-email"]').type('jonah@gmail.com');
-      cy.get('[data-cy="login-input-password"]').type('1234');
-      cy.get('[data-cy="login-btn-submit"]').click();
-  
-      cy.get('.btn-primary').contains('View Products').click();
-      cy.get('.product-list button').first().click();
-      
-      cy.get('#cartItems').should('contain', 'product 4');
-    });
-    it('Should display a message after checking out', () => {
+describe("E-commerce Application Tests", () => {
+  const user = {
+    name: "jonathan ndambuki",
+    email: "jonah@gmail.com",
+    password: "1234",
+  };
 
-      cy.get('[data-cy=login-input-email]').type('jonah@gmail.com');
-      cy.get('[data-cy=login-input-password]').type('1234');
-      cy.get('[data-cy=login-btn-submit]').click();
-  
-      cy.get('.btn-primary').contains('View Products').click();
-      cy.get('.product-list button').first().click(); 
-
-      cy.get('#cart .btn-primary').contains('Checkout').click(); 
-      cy.get('#checkoutDetails').should('contain', 'Thank you for shopping with us!');
-    });
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
   });
-  
+
+  const registerUser = () => {
+    cy.get('[data-cy="register-btn-submit"]').click();
+    cy.get('[data-cy="register-input-name"]').type(user.name);
+    cy.get('[data-cy="register-input-email"]').type(user.email);
+    cy.get('[data-cy="register-input-password"]').type(user.password);
+    cy.get('[data-cy="register-btn-submit"]').click();
+  };
+
+  const loginUser = () => {
+    cy.get('[data-cy="login-input-email"]').type(user.email);
+    cy.get('[data-cy="login-input-password"]').type(user.password);
+    cy.get('[data-cy="login-btn-submit"]').click();
+  };
+
+  it("Should register a new user", () => {
+    registerUser();
+    cy.get("#userPanel").should("be.visible");
+  });
+
+  it("Should log in an existing user", () => {
+    registerUser();
+    loginUser();
+    cy.get("#userPanel").should("be.visible");
+  });
+
+  it("Should add items to the cart", () => {
+    loginUser();
+    cy.get(".btn-primary").contains("View Products").click();
+    cy.get(".product-list button").first().click();
+
+    cy.get("#cartItems").should("contain", "product 4");
+  });
+
+  it("Should display a message after checking out", () => {
+    loginUser();
+    cy.get(".btn-primary").contains("View Products").click();
+    cy.get(".product-list button").first().click();
+    cy.get("#cart .btn-primary").contains("Checkout").click();
+
+    cy.get("#checkoutDetails").should(
+      "contain",
+      "Thank you for shopping with us!"
+    );
+  });
+});
